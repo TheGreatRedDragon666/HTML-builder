f7b01096c13143c1bc113db5910cceec51d1f6ae
Kristina Mnatsakanian <kristina.m1999@yandex.ru>
2023-05-07 01:21:46 +0600

    feat: implement 3rd task solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout} = process;
+
+const pathToSecretFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathToSecretFolder, (err, files) => {
+    files.forEach(file => {
+        const newPath = path.join(__dirname, 'secret-folder', file);
+        fs.stat(newPath, (err, stats) => {
+            if (stats.isFile()) {
+                const size = stats.size + 'b';
+                const ext = path.extname(newPath).slice(1);
+                const [ fileName ] = file.split('.');
+                stdout.write(`${fileName} - ${ext} - ${size} \n`)
+            }
+        })
+    })
+})
