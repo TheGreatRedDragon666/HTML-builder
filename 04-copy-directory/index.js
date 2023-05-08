9f201244c211ebe272edfeff4f7c51625a0250c5
Kristina Mnatsakanian <kristina.m1999@yandex.ru>
2023-05-08 17:01:11 +0600

    feat: implement 4th task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+const handleErr = function(err) {
+    if (err) {
+        throw err
+    }
+}
+
+function copyDir(dirname) {
+    const pathToFiles = path.join(__dirname, dirname);
+    const pathToTargetDir = path.join(__dirname, `${dirname}-copy`);
+    fs.rm(pathToTargetDir, {recursive: true, force: true}, err => {
+        if (err) handleErr();
+        fs.mkdir(pathToTargetDir, {recursive: true}, handleErr);
+        fs.readdir(pathToFiles, (err, files) => {
+            files.forEach(file => {
+                    const pathToSourceFile = path.join(pathToFiles, file);
+                    const pathToTargetFile = path.join(pathToTargetDir, file);
+                    fs.copyFile(pathToSourceFile, pathToTargetFile, handleErr);
+            });
+        });
+    });
+}
+
+copyDir('files');
